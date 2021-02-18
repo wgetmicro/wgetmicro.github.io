fafd1f36943d87af0d073a111762d7f1fcb91671
deni2020 <deni2020@abv.bg>
2021-02-19 00:51:46 +0200

    2021-02-19T00:51:46+02:00

--- ci/runDockerInner.sh
+++ ci/runDockerInner.sh
@@ -2,19 +2,7 @@
 
 set -eu
 
-if [ -x "$(command -v apt-get)" ]; then
-  # Debian/Ubuntu
-  # 	apt-get update
-  # apt-get install -y curl
-elif [ -x "$(command -v apk)" ]; then
-  # Alpine
-  # apk update
-  # apk add wget
-  # apk add libc6-compat
-elif [ -x "$(command -v pkg)" ]; then
-  # FreeBSD
-  pkg install wget
-fi
+[ -x "$(command -v pkg)" ] && pkg install wget
 
 cd /app
 "$1" ./ci/runTest.sh "$1"
