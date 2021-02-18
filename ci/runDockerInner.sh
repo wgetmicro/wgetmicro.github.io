2118328d049f6a86cca917c8e2e3710cca13ac1a
deni2020 <deni2020@abv.bg>
2021-02-19 01:11:48 +0200

    2021-02-19T01:11:48+02:00

--- ci/runDockerInner.sh
+++ ci/runDockerInner.sh
@@ -2,7 +2,13 @@
 
 set -eu
 
-[ -x "$(command -v pkg)" ] && pkg install wget
+if [ -x "$(command -v apt-get)" ]; then
+  # Debian/Ubuntu
+  apt-get update
+  apt-get install -y wget
+elif [ -x "$(command -v pkg)" ]; then
+  pkg install wget
+fi
 
 cd /app
 "$1" ./ci/runTest.sh "$1"
